f32d5c9c4ccd8454dc1d7d96678197f6fe4fd9a8
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-04-22 13:15:49 -0300

    Setup calendar

--- NoJones/Controller/ProgressViewController.swift
+++ NoJones/Controller/ProgressViewController.swift
@@ -7,12 +7,72 @@
 //
 
 import UIKit
+import CVCalendar
 
 class ProgressViewController: UIViewController {
-
+    
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var calendarView: CVCalendarView!
+    @IBOutlet weak var monthLabel: UILabel!
+    private var currentCalendar: Calendar?
+    
+    override func awakeFromNib() {
+        let timeZoneBias = 480 // (UTC+08:00)
+        currentCalendar = Calendar(identifier: .gregorian)
+        currentCalendar?.locale = Locale(identifier: "fr_FR")
+        if let timeZone = TimeZone(secondsFromGMT: -timeZoneBias * 60) {
+            currentCalendar?.timeZone = timeZone
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        
+        if let currentCalendar = currentCalendar {
+            monthLabel.text = CVDate(date: Date(), calendar: currentCalendar).globalDescription
+
+        }
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        menuView.commitMenuViewUpdate()
+        calendarView.commitCalendarViewUpdate()
     }
 
 
+    func setupViews() {
+        menuView.backgroundColor = .none
+        calendarView.backgroundColor = .none
+    }
+}
+
+typealias CalendarDelegates = CVCalendarMenuViewDelegate & CVCalendarViewDelegate &
+CVCalendarViewAppearanceDelegate
+
+extension ProgressViewController: CalendarDelegates {
+    
+    // MARK: Configuration Calendar
+    
+    func presentationMode() -> CalendarMode { return .monthView }
+    
+    func firstWeekday() -> Weekday { return .sunday }
+    
+    func presentedDateUpdated(_ date: CVDate) {
+        self.monthLabel.text = date.globalDescription
+    }
+    
+    func shouldShowWeekdaysOut() -> Bool {
+        true
+    }
+    
+    // MARK: Appearance Calendar
+    
+    func dayLabelWeekdayDisabledColor() -> UIColor { return .lightGray }
+       
+    func dayOfWeekTextColor() -> UIColor { return .gray }
+    
+    
 }
