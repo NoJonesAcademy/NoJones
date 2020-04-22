539a0cfcbc08e5a345556ad3656516d5001993f5
Albert Rayneer <rayneerqueiroz@gmail.com>
2020-04-22 15:50:56 -0300

    Addiction and achievement structures added

---
+++ NoJones/Controller/Addiction.swift
@@ -0,0 +1,15 @@
+//
+//  Addiction.swift
+//  NoJones
+//
+//  Created by Albert Rayneer on 22/04/20.
+//  Copyright © 2020 NoJones. All rights reserved.
+//
+
+import Foundation
+
+struct Addiction {
+    var name: String?
+    var days: Int?
+    var type: String?
+}
