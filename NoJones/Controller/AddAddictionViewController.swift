e1dd72a4230b19ed3ef1d0a450ccdf1850582300
NobreMateus <40170572+NobreMateus@users.noreply.github.com>
2020-04-22 10:01:15 -0300

    Main elements added

--- NoJones/Controller/AddAddictionViewController.swift
+++ NoJones/Controller/AddAddictionViewController.swift
@@ -12,8 +12,7 @@ class AddAddictionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
     }
     
 
