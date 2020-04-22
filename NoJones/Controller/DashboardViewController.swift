186437f4f3fc703d39f6a2a4af81719248f5b0c5
Albert Rayneer <rayneerqueiroz@gmail.com>
2020-04-22 15:15:32 -0300

    Empty table view image added

--- NoJones/Controller/DashboardViewController.swift
+++ NoJones/Controller/DashboardViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UITableViewDataSource {
 
+    var addictions = [String]()
+//    var addictions = ["Addiction",
+//    "Addiction",
+//    "Addiction",
+//    "Addiction",]
+    
+
+    @IBOutlet weak var noAddictionsImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        if addictions.isEmpty {
+            tableView.backgroundColor = .none
+            tableView.separatorStyle = .none
+        } else {
+            noAddictionsImage.isHidden = true
+        }
+        
+        tableView.isHidden = false
+        tableView.rowHeight = 70
+        
+                
+        tableView.dataSource = self
         
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addictions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "addictionCell")
+        
+        cell.textLabel?.text = addictions[indexPath.row]
+        
+        return cell
+    }
+    
 }
