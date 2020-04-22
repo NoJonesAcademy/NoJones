3ec97726aa18a1e8cc3ef930e640b763dd69cd5c
NobreMateus <40170572+NobreMateus@users.noreply.github.com>
2020-04-22 19:06:41 -0300

    Textfiled styles ajusted

--- NoJones/Controller/AddAddictionViewController.swift
+++ NoJones/Controller/AddAddictionViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class AddAddictionViewController: UIViewController {
-
+@available(iOS 13.0, *)
+class AddAddictionViewController: UIViewController, UIPickerViewDelegate, UITextFieldDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var addictionPicker: UIPickerView!
+    var addictionData:[String] = [String]()
+    
+    @IBOutlet weak var addictionNameTextField: UITextField!
+    @IBOutlet weak var newHabitTextField: UITextField!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return addictionData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       return addictionData[row]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.configueTextFields()
+        
+        self.addictionPicker.delegate = self
+        self.addictionPicker.dataSource = self
+        addictionData = ["Cigarros","Drogas","Jogos"]
+        
+    }
+    
+    func configueTextFields(){
+        
+        self.addictionNameTextField.delegate = self
+        self.newHabitTextField.delegate = self
+        
+        
+       self.addictionNameTextField.layer.borderWidth  = 2
+       self.addictionNameTextField.layer.cornerRadius = 8
+       self.addictionNameTextField.layer.borderColor = .init(srgbRed: 27/255, green: 88/255, blue: 166/255, alpha: 1)
+       
+       self.newHabitTextField.layer.borderWidth  = 2
+       self.newHabitTextField.layer.cornerRadius = 8
+       self.newHabitTextField.layer.borderColor = .init(srgbRed: 27/255, green: 88/255, blue: 166/255, alpha: 1)
     }
     
 
