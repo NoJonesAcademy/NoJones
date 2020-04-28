d3402c763b75037fd725b3d6cdc7c9104c1f67c4
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-04-28 15:40:21 -0300

    Changes in Calendar and adding HabitDetails

---
+++ NoJones/Controller/HabitDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HabitDetailsViewController.swift
+//  NoJones
+//
+//  Created by VInicius Mesquita on 28/04/20.
+//  Copyright © 2020 NoJones. All rights reserved.
+//
+
+import UIKit
+
+class HabitDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
