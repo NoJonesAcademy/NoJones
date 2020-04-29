dfcd8b3e03f3a48dd5de0aa74eb92b05ced41c7d
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-04-29 11:43:34 -0300

    System BackgroundColor

--- NoJones/Controller/HabitDetailsViewController.swift
+++ NoJones/Controller/HabitDetailsViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class HabitDetailsViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var borderView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        borderView.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        let views = stackView.arrangedSubviews
+        
+        for view in views {
+            view.layer.cornerRadius = 12.5
+        }
         // Do any additional setup after loading the view.
     }
     
