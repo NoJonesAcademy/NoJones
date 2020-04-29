194ef69341ccff12c53917e9a13a078962182175
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-04-29 15:54:21 -0300

    Habits changes

--- NoJones/Controller/HabitDetailsViewController.swift
+++ NoJones/Controller/HabitDetailsViewController.swift
@@ -16,24 +16,16 @@ class HabitDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        borderView.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        configureViews()
+    }
+    
+    func configureViews() {
+        borderView.layer.borderColor = UIColor.init(named: "secondaryColor")?.cgColor
         let views = stackView.arrangedSubviews
         
         for view in views {
             view.layer.cornerRadius = 12.5
         }
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
